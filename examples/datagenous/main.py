abd13801e83815af8f3b05e70f3ca7ac4ddbd2ff
root <root@de8>
2019-07-01 07:10:34 +0200

    adds demo ml server connection for csv

--- examples/datagenous/main.py
+++ examples/datagenous/main.py
@@ -6,12 +6,14 @@ run ``python main.py``.
 """
  Will be object oriented in the future
 """
+import requests
+import os
 import logging
-import simplejson as json
+import simplejson as sjson
+import json
 from functools import wraps
 from flask import Flask, request, jsonify, abort,send_from_directory,render_template
 from os.path import join
-import os
 
 
 
@@ -22,6 +24,10 @@ HDLR = logging.FileHandler('DatagenousRouter.log')
 FORMATTER = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 HDLR.setFormatter(FORMATTER)
 LOGGER.addHandler(HDLR)
+
+# Set ml server path
+ML_SERVER_PATH="http://localhost:5000"
+
 
 app = Flask(__name__, static_url_path='/static')
 app.config['DEBUG'] = True
@@ -68,6 +74,24 @@ def canvas():
     """ Returns if server is alive  """
     LOGGER.info("Received heartbeat check from " +  get_request_ip())
     return app.send_static_file('index.html')
+
+
+
+# Analytics API callers over HTTP
+@app.route('/canvas/api/read_csv/<string:filename>', methods=["GET"])
+def get_csv(filename):
+    url = ML_SERVER_PATH + "/demo/api/v1.0/previewfile/"+ filename + "/csv"
+    headers = {
+        'Accept': "*/*",
+        'Cache-Control': "no-cache",
+        'Host': ML_SERVER_PATH,
+        'accept-encoding': "gzip, deflate",
+        'Connection': "keep-alive",
+        'cache-control': "no-cache"
+    }
+
+    response = requests.request("GET", url, headers=headers)
+    return json.dumps(response.text)
 
 
 if __name__ == '__main__':
