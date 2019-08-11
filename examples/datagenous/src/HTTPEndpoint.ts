31414d03a7ced5a45eb9e31aba11a2508e3c0458
Semih Korkmaz <skorkmaz88@yahoo.com>
2019-08-11 03:29:53 -0700

    HTTP endpoint widget

---
+++ examples/datagenous/src/HTTPEndpoint.ts
@@ -0,0 +1,79 @@
+import { Widget } from '@phosphor/widgets';
+import { Message, IMessageHandler, MessageLoop } from '@phosphor/messaging';
+import { v4 as uuid } from 'uuid';
+import { CustomMessage } from './CustomMessage';
+
+export class HTTPEndpoint extends Widget {
+  id: string;
+  // Data source ids
+  children_ids: string[];
+  // Accepted children types
+  children_types: string[];
+  //
+  children_messages: string[];
+  //
+  widget_type: string;
+
+  constructor() {
+    super({ node: HTTPEndpoint.createNode(uuid()) });
+  }
+
+  static createNode(widget_id: string): HTMLElement {
+    // Create two widgets, delete me and make me dynamic later
+    let node = document.createElement('div');
+    node.id = widget_id;
+
+    node.innerHTML =
+      '<div class="head">' +
+      '<div class="left">Input/HTTP/' +
+      '</div>' +
+      '<div class="right">' +
+      '<a id="play-button" onclick="draw_line_chart(' +
+      widget_id +
+      ');" href="" >' +
+      '<i class="icon-play"></i></a>' +
+      '<a href="#" class="close"><i class="icon-exit"></i></a>' +
+      '</div>' +
+      '</div>' +
+      '<div class="body">' +
+      '<input id = "httpendpoint" type="text" name="httpendpoint" placeholder="HTTP API endpoint">' +
+      '<br/>' +
+      '<br/>' +
+      '<input id = "httpendpointinterval" type="text" name="httpendpointinterval" placeholder="Interval in seconds">' +
+      '</div>';
+
+    node.setAttribute('class', 'window');
+    node.setAttribute(
+      'onMouseUp',
+      'get_window_position_state("' + widget_id + '")'
+    );
+    return node;
+  }
+
+  public createInstance(): void {
+    let body = document.body;
+    window.console.log('Creating HTTP endpoint widget');
+    // Get canvas
+    let canvas = document.getElementById('graphContainer');
+    // First Menu
+    Widget.attach(this, canvas);
+    (window as any).eval('visualize()');
+  }
+
+  // bind message system
+  messageHook(handler: IMessageHandler, msg: Message): boolean {
+    if (handler === this) {
+      this.processMessage(msg);
+    }
+    return true;
+  }
+
+  processMessage(msg: Message): void {
+    window.console.log(msg);
+    if (msg instanceof CustomMessage) {
+      let parameter = (<CustomMessage>msg).content;
+      let sending_child = (<CustomMessage>msg).child_id;
+      let sending_child_type = (<CustomMessage>msg).child_type;
+    }
+  }
+}
